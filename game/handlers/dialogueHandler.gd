0c5c8d483da79429b5b815c2d05484f8488b3d9f
ChipsRisu <chipsrisu@gmail.com>
2026-08-13 23:08:37 +0200

    Dialog handler refactor

--- game/handlers/dialogueHandler.gd
+++ game/handlers/dialogueHandler.gd
@@ -3,7 +3,14 @@ extends Node
 func start(interaction: Interaction) -> void:
 	# TODO maybe some logic here ? 
 	# Or else we can just skip this handler and use Dialogic directly, since it's already a Singleton itself !
-	print("INTERACTION TRIGGERED > ", interaction.key)
-	
+	var id = workingId(interaction)
 	interaction.checked = true
-	Dialogic.start(interaction.key)
+	
+	# For Dev purpose only
+	print("INTERACTION TRIGGERED > ", id)
+	
+	if Dialogic.timeline_exists(id):
+		Dialogic.start(id)
+
+func workingId(interaction: Interaction) -> String:
+	return "%s_%s_%s" % [interaction.key, GameHandler.state.situation, GameHandler.state.day]
