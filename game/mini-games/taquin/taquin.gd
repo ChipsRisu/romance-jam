45ce7a538496e454a27e313cd3c288b14a862878
Vulcainos <Vulcainos@users.noreply.github.com>
2026-08-16 22:57:44 +0200

    Ajout des images pour le taquin et animation fin

--- game/mini-games/taquin/taquin.gd
+++ game/mini-games/taquin/taquin.gd
@@ -3,10 +3,12 @@ extends Node2D
 var tiles: Array[Sprite2D] = [];
 var solved: Array[Sprite2D] = [];
 var emptyTile: Sprite2D;
+var gameIsFinish = false;
 
 #region setup
 func _ready() -> void:
 	tiles = [$Tile5, $Tile8, $Tile2, $Tile1, $Tile9, $Tile3, $Tile4, $Tile7, $Tile6];
+	#tiles = [$Tile1, $Tile2, $Tile3, $Tile4, $Tile5, $Tile6, $Tile7, $Tile9, $Tile8]; #Pour le test
 	
 	solved = [$Tile1, $Tile2, $Tile3, $Tile4, $Tile5, $Tile6, $Tile7, $Tile8, $Tile9];
 	#tiles.shuffle(); PAS faire de random car solution pas forcément possible (faq de https://www.dcode.fr/solveur-taquin-3x3)
@@ -17,13 +19,15 @@ func placeTiles() -> void:
 	for index in range(tiles.size()):
 		var tile: Sprite2D = tiles[index];
 		
-		tile.position.x = ((index%3)-1) * 250;
-		tile.position.y = (((index/3) as int)-1) * 250;
+		tile.position.x = ((index%3)-1) * 300;
+		tile.position.y = (((index/3) as int)-1) * 300;
 		#print(tile.position, tile.name)
 #endregion
 
 #region tiles methodes
 func _input(event) -> void:
+	if gameIsFinish or not visible:
+		return;
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		#print(event);
 		var mousePosition: Vector2 = (event as InputEventMouseButton).global_position - get_viewport_rect().size /2;
@@ -59,7 +63,13 @@ func swap(row:int, col: int, EmptyRow:int, EmptyCol: int) -> void:
 	#Les positions des tiles
 	var swapTile: Sprite2D = tiles[swapTileIndex];
 	var tempPos: Vector2 = swapTile.position;
-	swapTile.position = emptyTile.position;
+	
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_LINEAR)
+	tween.set_ease(Tween.EASE_IN)
+	await tween.tween_property(swapTile, "position", emptyTile.position, 0.2)
+	
+	#swapTile.position = emptyTile.position ;
 	emptyTile.position = tempPos;
 	
 	#Le placement dans le tableau
@@ -70,7 +80,10 @@ func swap(row:int, col: int, EmptyRow:int, EmptyCol: int) -> void:
 #endregion
 
 func GameIsWin() -> void:
-	print("Bravo le jeu est gagné");
+	gameIsFinish = true;
+	$AnimationPlayer.play("finishGame");
+	await $AnimationPlayer.animation_finished;
+	#TODO faire la suite pour sortire du minijeu. Pas oublié de queue free le jeu ou le rendre visible false;
 
 
 #region private methode
@@ -80,18 +93,18 @@ func detectIfWin() -> void:
 
 func getRowFrom(y: float) -> int:
 	var row = 0;
-	if y < -125:
+	if y < -150:
 		row = -1;
-	elif  y > 125:
+	elif  y > 150:
 		row = 1;
 	return row;
 
 	
 func getColFrom(x: float) -> int:
 	var col = 0;
-	if x < -125:
+	if x < -150:
 		col = -1;
-	elif  x > 125:
+	elif  x > 150:
 		col = 1;
 	return col;
 #endregion
