fb0044ca3b578b20c42c666ff42cdb76d1837fec
ChipsRisu <chipsrisu@gmail.com>
2026-08-24 02:39:41 +0200

    Added wip music

--- game/main.gd
+++ game/main.gd
@@ -3,7 +3,6 @@ extends Node
 var arrorCursor = load("res://assets/UI/cursor 1-little.png");
 var pointerCursor = load("res://assets/UI/cursor 2-little.png");
 
-
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(arrorCursor,  Input.CURSOR_ARROW);
 	Input.set_custom_mouse_cursor(pointerCursor, Input.CURSOR_POINTING_HAND);
@@ -13,3 +12,6 @@ func _ready() -> void:
 
 func _on_toggle_movement_toggled(toggled_on: bool) -> void:
 	SituationHandler.toggleMovement(toggled_on)
+
+func _on_audio_stream_player_finished() -> void:
+	$AudioStreamPlayer.play()
